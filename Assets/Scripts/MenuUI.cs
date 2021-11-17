889f84c83c815acbb9f06f5ed512bcbb3b934bf8
MSTGameDesignClass <93927548+MSTGameDesignClass@users.noreply.github.com>
2021-11-17 09:30:23 -0500

    Updates

---
+++ Assets/Scripts/MenuUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine.SceneManagement;
+
+public class MenuUI : MonoBehaviour
+{
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+
+#endif
+    }
+}
